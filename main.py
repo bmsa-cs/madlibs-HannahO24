e656d181e85033eae76f3ffe0b35c1458c23f0da
Hannah Orris <horris24@biomedscienceacademy.org>
2020-11-10 15:04:20 +0000

    I fixed some spacing errors and added an if/else statement to determine the character's gender.

--- main.py
+++ main.py
@@ -3,34 +3,40 @@ MadLibs
 Author: Hannah Orris
 Period/Core: 5
 """
-noun = input("Enter a noun: ") #used
-name = input("Enter a name: ") #used
-verb = input("Enter a verb(in past tense): ") #used
-place = input("Enter a place: ") #used
-adjective = input("Enter a adjective: ") #used
+gender = int(input("Type 1 for boy or 2 for girl: "))
+name = input("Enter a name: ") 
+teacher = input("Enter a teacher name: ") 
+place = input("Enter a place: ")
+verb = input("Enter a verb(in past tense): ") 
+person = input("Enter a famous person: ")
+adjective = input("Enter a adjective: ") 
+noun = input("Enter a noun: ") 
 beverage = input("Enter a beverage: ")
-person = input("Enter a famous person: ") #used
-noun2 = input("Enter another noun: ") #used
-obj = input("Enter a strange object: ") #used
-print("   ")
-#Prompts match up with the order the words are used in the story.
-#At least 8 inputs/variables
-#Inputs should use a variety of parts of speech (noun, adjective, verb, etc.) and/or specific types of nouns (place, food, etc.)
+noun2 = input("Enter another noun(preferably an object): ") 
+obj = input("Enter a strange object: ") 
 
-#Has proper spelling and grammar
-#Is at least 6 sentences/lines long.
+print("   ") #anytime this is used, a space is added to make the story easier to read.
 
-#At least one print() statement that outputs on multiple lines (ie. uses "\n")
-#At least one print() statement that concatenates (“adds”) two strings together (eg. "Hello " + "world")
-#At least one f-string or .format() that correctly inserts the variables.
-
-print("   ")
+print("   ") 
+print("   ") 
 print("This is a short story based off your input.")
 print("   ")
-print(name + " was on their way to history class.")
-print("When they got there, their teacher, Mr. Smith said," "\n" "    Today we are going to learn about the history of " + place +"!")
-print("The whole class " + verb + ".")
-print("The whole class decided to ditch and watch a youtube documentery about " + person + ".")
-print("As they were watching the video, a " + adjective + noun +" walked over holding a " + beverage + " in one arm and a " + noun2 +" in the other." "\n" "The " + adjective + noun + "said,")
-print("    \"Hey, you guys wanna come over to my basement? I have a really cool " + obj + "!\"")
+print("   ") 
+
+if gender == 1:
+  print(f"It was a beautiful Tuesday morning. {name} was on his way to history class.")
+  print(f"When he got there, his teacher, {teacher} said, \n    \"Today we are going to learn about the history of {place}!\"")
+else:
+  print(f"It was a beautiful Tuesday morning. {name} was on her way to history class.")
+  print(f"When she got there, her teacher, {teacher} said, \n    \"Today we are going to learn about the history of {place}!\"")
+
+print("   ") 
+
+print("The whole class " + verb + " in anger.")
+print("They all decided to ditch class and watch a youtube documentery about " + person + ".")
+
+print("   ") 
+
+print(f"As they were watching the video, a {adjective} {noun} walked over holding a {beverage} in one arm and a {noun2} in the other. \nThe {adjective} {noun} said,")
+print(f"    \"Hey, you guys wanna come over to my basement? I have a really cool {obj}!\"")
 print("    \"Sure! The class replied.\"")
